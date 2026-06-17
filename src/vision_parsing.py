b7117d241f8ee090b52a9e9a6206501c6fb01d43
Lida Wen <liendaw7419@gmail.com>
2026-06-17 15:57:26 +0800

    deleted unneeded output message from extract_images file

--- src/vision_parsing.py
+++ src/vision_parsing.py
@@ -1,8 +1,15 @@
+#converts binary data (images, audio) into ASCII charaters
+#travel through text-based system without corruption
 import base64
 from environment import model
 
 def parse_image(image_data, ext="jpeg"):
+    #convert raw image bytes into base64 bytes
+    #now bytes object but with ASCII char in it
+    #.decode() converts from bytes into Python string
     encoded = base64.b64encode(image_data).decode()
+    #langchain calls llm imported from environment
+    #with images as base64 encoded string representation
     response = model.invoke([{
         "role": "human",
         "content": [{
@@ -18,7 +25,6 @@ def parse_image(image_data, ext="jpeg"):
 if __name__ == "__main__":
     from extract_images import extract_images_from_pdf
     images = extract_images_from_pdf("data/TS-SCERT-Class-10-Biology-Textbook-in-English-Medium.pdf")
-    print(f"Found {len(images)} images")
     if images:
         img = images[0]
         print(f"Testing image from page {img['page']}, format={img['ext']}")
