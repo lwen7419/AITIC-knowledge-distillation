8b5cb33c409faa38d1fec6df41c424a7dffb3741
Lida Wen <liendaw7419@gmail.com>
2026-06-29 18:45:29 +0800

    added comments for reranker

--- src/reranker.py
+++ src/reranker.py
@@ -10,24 +10,42 @@ class CrossEncoderReranker:
         #loads tokenizer for model from HuggingFace, loaded into instance attribute 
         # tokenizer
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        #downloads and loads model weights from HuggingFace
         self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        #switch model to evaluation mode
         self.model.eval()
-
+    #take search query and list of (Document,score) tuples returned by ChromaDB
     def rerank(self, query, hits):
+        #builds list of (query, chunk_text) pairs, throw away previous embedding 
+        # scores
         pairs = [(query, doc.page_content) for doc, _ in hits]
-
+        #tokenize all pairs in single batch 
         features = self.tokenizer(
+            #first list is all queries, second list is chunk texts
             [p[0] for p in pairs],
             [p[1] for p in pairs],
+            #make all inputs same length by adding padding tokens to shorter ones
             padding=True,
+            #cut off anything larger than max_length, which is max tokens model 
+            #can handle at once
             truncation=True,
             max_length=512,
+            #return PyTorch tensors instead of Python lists
             return_tensors="pt"
         )
 
+        #tell PyTorch to not track gradients during rerank. Gradients needed
+        #for update weights in training, skipping makes inference faster and 
+        #uses less memory
         with torch.no_grad():
+            #runs huggingface model, getting raw output scores and removing 
+            # last dimension
             scores = self.model(**features).logits.squeeze(-1)
 
+        #zip original hit with cross-encoder score to sort highest score first
+        # and converts Pytorch tensor into Python list so zip can work with
         scored = sorted(zip(hits, scores.tolist()), key=lambda x: x[1], reverse=True)
 
+        #return just hits in new reranked order, discarding cross-encoder scores,
+        # keeping original (Document, chromadb_score) format
         return [hit for hit, _ in scored]
